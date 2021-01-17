9ce5c446343580f1dd3e73ece1f531fc61476e77
dwildcash <dwildcash@prontomail.com>
2021-01-17 16:07:44 -0500

    fix

--- shift-dashboard/Program.cs
+++ shift-dashboard/Program.cs
@@ -17,21 +17,6 @@ namespace shift_dashboard
                 {
                     webBuilder.UseUrls("http://localhost:6000"); // Configure on port 6000 instead of default
                     webBuilder.UseStartup<Startup>();
-
-                    webBuilder.ConfigureAppConfiguration((ctx, cb) =>
-                    {
-                        // 👇 Please specify the condition that is true only when
-                        //    the application is running on your development environment.
-                        //    Notice that excludes the case that the environment is "Development".
-                        if (!ctx.HostingEnvironment.IsDevelopment())
-                        {
-                            // 👇 This call inserts "StaticWebAssetsFileProvider" into
-                            //    the static file middleware.
-                            StaticWebAssetsLoader.UseStaticWebAssets(
-                              ctx.HostingEnvironment,
-                              ctx.Configuration);
-                        }
-                    });
                 });
     }
 }
